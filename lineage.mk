93a2dc3fa6e01668b36e5e207393ab904490b37a
Alberto97 <albertop2197@gmail.com>
2017-01-01 22:53:15 +0100

    addison: Rebrand to LineageOS

--- lineage.mk
+++ lineage.mk
@@ -20,7 +20,7 @@ TARGET_SCREEN_HEIGHT := 1920
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := addison
-PRODUCT_NAME := cm_addison
+PRODUCT_NAME := lineage_addison
 PRODUCT_BRAND := motorola
 PRODUCT_MANUFACTURER := motorola
 
