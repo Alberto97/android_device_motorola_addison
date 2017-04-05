0e2a9fcf63a42ce0ba7a3323a8453625f9b083de
Jaekyun Seok <jaekyun@google.com>
2017-04-05 11:23:26 +0900

    Enforce RRO on framework-res
    Overlays only for framework-res will be converted into RROs.
    
    Other overlays can't be converted due to some known issues on app RRO.
    
    Bug: 36231603
    Change-Id: Idca30fdbbcc990fd124de16a06a112a346612a61
    (cherry picked from commit db1b6058200fc5255f519cf6609fca7619d6743e)

--- device.mk
+++ device.mk
@@ -19,6 +19,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
+PRODUCT_ENFORCE_RRO_TARGETS := framework-res
+
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
