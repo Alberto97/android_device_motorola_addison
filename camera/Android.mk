d2dc35d73c5933c649dddc655d87b99e8fe06270
Alberto97 <albertop2197@gmail.com>
2017-08-22 14:19:59 +0200

    addison: camera: Build fix

--- camera/Android.mk
+++ camera/Android.mk
@@ -7,7 +7,7 @@ LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils libcutils
+    libhardware liblog libcamera_client libutils libcutils libbase libgui
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
