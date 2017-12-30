3576ab14fc00665685bc297f40b1a5fb5f87b924
Alberto97 <albertop2197@gmail.com>
2017-12-30 19:05:10 +0100

    Build a 32bit version of sensorservice
    system_server is currently responable of starting SensorService and
    since it is a 64bit instance, system looks for 64bit sensorhal.
    Disable this mechanism and manually start SensorService with a 32bit
    copy of sensorservice to force the system using the 32bit HAL
    
    This is based on vache's work on potter

--- device.mk
+++ device.mk
@@ -280,6 +280,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
 # Sensors
+PRODUCT_PACKAGES += \
+    sensorservice_32 \
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:system/etc/sensors/hals.conf
 
