40a24a4ddf0635d558c0c38badd8332e5bf3e7e1
Alberto97 <albertop2197@gmail.com>
2018-01-20 13:31:57 +0100

    addison: Setup OSS Sensor HAL build

--- device.mk
+++ device.mk
@@ -275,11 +275,15 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
 # Sensors
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
-
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
+    libsensorhub \
+    motosh \
+    sensorhub.msm8953 \
+    sensors.msm8953
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
 # Thermal
 PRODUCT_COPY_FILES += \
