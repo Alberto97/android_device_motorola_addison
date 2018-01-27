0eea29de4883769a6611c97643f7450253f8973a
Alberto97 <albertop2197@gmail.com>
2018-01-27 21:30:31 +0100

    Revert "addison: Deny Moto Camera installation from the Play Store"
     * Moto apparently fixed their permissions mess. An Oreo version
       of the camera can be installed as user app and used without issues
     * This reverts commit dbdff96d1532390a8d79f6f8e74c6157a174727e.

--- device.mk
+++ device.mk
@@ -52,6 +52,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# Motorola Camera permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/com.motorola.cameraone.xml:system/etc/permissions/com.motorola.cameraone.xml
+
 # Screen density
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_AAPT_CONFIG := normal
