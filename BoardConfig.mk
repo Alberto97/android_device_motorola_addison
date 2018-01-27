210b980d9e2075d79a06a619414453f1798a4196
Alberto97 <albertop2197@gmail.com>
2018-01-27 20:57:06 +0100

    Enable LLVM kernel building

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -65,10 +65,9 @@ BOARD_KERNEL_LZ4C_DT := true
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
-TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CONFIG := addison_defconfig
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8953
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
 LZMA_RAMDISK_TARGETS := recovery
 
 # Audio
