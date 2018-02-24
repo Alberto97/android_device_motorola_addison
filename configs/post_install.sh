26938620bde0e8ceb2f2fdf603783e656ccbe4e6
Alberto97 <albertop2197@gmail.com>
2018-02-24 15:25:57 +0100

    Move NFC configs to vendor

--- configs/post_install.sh
+++ configs/post_install.sh
@@ -3,6 +3,6 @@
 variant=`getprop ro.boot.radio`
 
 if [ $variant == "China" ]; then
-    rm /system/etc/libnfc-nxp.conf
-    mv /system/etc/libnfc-nxp_ds.conf /system/etc/libnfc-nxp.conf
+    rm /system/vendor/etc/libnfc-nxp.conf
+    mv /system/vendor/etc/libnfc-nxp_ds.conf /system/vendor/etc/libnfc-nxp.conf
 fi
