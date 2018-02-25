e7bdb8e5737c00aa316cb8c31343c9587af3763a
Alberto97 <albertop2197@gmail.com>
2018-02-25 20:09:30 +0100

    Bump to Oreo fingerprint
     * Good enough, only radio stack is missing due to broken incall audio

--- aosp_addison.mk
+++ aosp_addison.mk
@@ -25,6 +25,6 @@ PRODUCT_MANUFACTURER := motorola
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST := ro.product.model
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=motorola/addison/addison:7.1.1/NPNS26.118-22-1/1:user/release-keys \
-    PRIVATE_BUILD_DESC="addison-user 7.1.1 NPNS26.118-22-1 1 release-keys" \
+    BUILD_FINGERPRINT=motorola/addison/addison:8.0.0/OPN27.76-12-2/4:user/release-keys \
+    PRIVATE_BUILD_DESC="addison-user 8.0.0 OPN27.76-12-2 4 release-keys" \
     PRODUCT_NAME="Moto Z Play"
