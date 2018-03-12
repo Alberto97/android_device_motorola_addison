5011e9e52c96fc0d353da46ecd0eba72d49bdcaf
Alberto97 <albertop2197@gmail.com>
2018-03-12 22:02:48 +0100

    Bring manifest up to date with built hals
     * Also add more graphics stuff

--- device.mk
+++ device.mk
@@ -130,8 +130,10 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
     android.hardware.renderscript@1.0-impl \
     gralloc.msm8953 \
     hwcomposer.msm8953 \
