0c4e1c03090f56bbc135864ddfb4260b2196149a
Alberto97 <albertop2197@gmail.com>
2018-03-30 15:37:36 +0200

    Add binderized audio service

--- device.mk
+++ device.mk
@@ -85,6 +85,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
+    android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
     audiod \
     audio.a2dp.default \
