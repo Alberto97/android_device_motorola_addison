53a98bbf2c96def2af739497845a0fda5041d303
Alberto97 <albertop2197@gmail.com>
2018-07-28 22:05:11 +0200

    Revert "Permissive SELinux again"
     * This reverts commit 432bed4a38c28cdeef247bbb119c3963f85d07ca.
     * Performance issues have been fixed

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -58,7 +58,6 @@ TARGET_OTA_ASSERT_DEVICE := addison,addison_retail
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 vmalloc=350M
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_IMAGE_NAME := Image.gz
 BOARD_KERNEL_LZ4C_DT := true
