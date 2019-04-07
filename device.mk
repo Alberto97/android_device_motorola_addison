ac972fffcc162c956ca84a153da81c2d5d261dd8
Alberto97 <albertop2197@gmail.com>
2019-04-07 20:49:01 +0200

    sensor: Import renamed HIDL HAL from android-9.0.0_r16

--- device.mk
+++ device.mk
@@ -271,7 +271,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-impl.motosh \
     libsensorhub \
     motosh \
     sensorhub.msm8953 \
